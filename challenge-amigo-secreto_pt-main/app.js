761a8c4598bdbc44c280cf80b9f7730f86e0a195
kassioramos <kassio.ramos03@gmail.com>
2025-03-04 18:00:58 -0300

    implementar uma função para sortear os amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -14,6 +14,29 @@ if(amigo.trim() !== '') {
  li.textContent = amigo;
  // Adicionar lista de amigos na página
  document.getElementById('listaAmigos').appendChild(li);
- 
-}    
+ // Limpar campode de entrada após adicionar o nome
+ document.getElementById('amigo').value = '';
+} else{
+ // Se o campo estiver vazio mostar uma menssagem de alerta
+ alert('Por favor, digite o nome de um amigo.');
+ return;   
+}
+}
+// Função para sortear um amigo aleatoriamente
+function sortearAmigo(){
+// Verifica se há amigos na lista
+if (amigos.length === 0) {
+    alert ('Não há amigos, por favor, adicionar pelomenos um amigo');
+    return;
+}  
+
+  // Sorteia um índice aleatório entre 0 e o tamanho da lista de amigos
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+  // Obtém o nome do amigo sorteado
+  const amigoSorteado = amigos[indiceAleatorio];
+  
+  // Exibe o nome do amigo sorteado na tela
+  const resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<li>O amigo sorteado é: <strong>${amigoSorteado}</strong></li>`;
 }
