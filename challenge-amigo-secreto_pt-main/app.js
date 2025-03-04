12ac40edaf57449b6f50a83216351efb6c6188ec
kassioramos <kassio.ramos03@gmail.com>
2025-03-04 16:27:24 -0300

    implementar função para agregar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,17 @@
 //Array para adicionar nome de amigos
 let amigos = [];
+//Função para adicionar nome de amigos a lista
+function adicionarAmigos() {
+//obtêm valor digitado no campo de entrada
+const amigo = document.getElementById("amigo").ariaValue;
+//Verificar se o campo setá vazio
+if(amigo.trim() !== '') {
+//adicionar nome do amigo no array
+    amigos.push(amigos);
+ //Criar um novo item de lista (li)
+ const li = document.createElement("li");
+ //Definir conteudo de ittêm de lista(li) como nome de amigo
+ li.textContent = amigo;
+
+}    
+}
