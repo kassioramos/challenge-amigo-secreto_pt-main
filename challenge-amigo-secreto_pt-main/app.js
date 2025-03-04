be2885a53f7a2b8570b87f7f6ec7799cf5b0687e
kassioramos <kassio.ramos03@gmail.com>
2025-03-04 17:22:00 -0300

    implementar uma função para atualizar lista de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,17 +1,19 @@
 //Array para adicionar nome de amigos
 let amigos = [];
 //Função para adicionar nome de amigos a lista
-function adicionarAmigos() {
-//obtêm valor digitado no campo de entrada
-const amigo = document.getElementById("amigo").ariaValue;
-//Verificar se o campo setá vazio
+function adicionarAmigo() {
+//obtém valor digitado no campo de entrada
+const amigo = document.getElementById('amigo').value;
+//Verificar se o campo está vazio
 if(amigo.trim() !== '') {
 //adicionar nome do amigo no array
-    amigos.push(amigos);
+    amigos.push(amigo);
  //Criar um novo item de lista (li)
- const li = document.createElement("li");
- //Definir conteudo de ittêm de lista(li) como nome de amigo
+ const li = document.createElement('li');
+ //Definir conteudo de item de lista(li) como nome de amigo
  li.textContent = amigo;
-
+ // Adicionar lista de amigos na página
+ document.getElementById('listaAmigos').appendChild(li);
+ 
 }    
 }
